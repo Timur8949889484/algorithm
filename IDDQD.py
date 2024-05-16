79b6e000db97b8d6ed1fd3a9422e2fe43a81da43
Timur8949889484 <adskiiidroilla@gmail.com>
2024-05-16 10:57:38 +0600

    Initial commit

---
+++ IDDQD.py
@@ -0,0 +1,38 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] < target:
+            low = mid + 1
+        elif arr[mid] > target:
+            high = mid - 1
+        else:
+            print(f"Элемент найден под индексом: {mid}")
+            return
+    print("Нету")
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+sorted_arr = bubble_sort(arr)
+print(f"Отсортирован пузырьковым методом: {sorted_arr}")
+
+target = 22
+binary_search(sorted_arr, target)
